b6f14177c339e8bfd72a4ba42824100b696190b8
qiusli <qius.li3@gmail.com>
2014-07-05 00:28:40 -0400

    another three problems

---
+++ Qiushi/GasStation.java
@@ -0,0 +1,14 @@
+/**
+ * Created by liqiushi on 7/5/14.
+ */
+public class GasStation {
+    public static void main(String[] args) {
+        int[] gas = {1, 2, 3, 4, 5, 6};
+        int[] cost = {1, 2, 3, 4, 5, 6};
+        canCompleteCircuit(gas, cost);
+    }
+
+    private static int canCompleteCircuit(int[] gas, int[] cost) {
+
+    }
+}
